211c0617af12bfe20eccb2b37f87d26f5025944e
Adi Mintz <azzminzz@gmail.com>
2022-03-01 13:41:00 +0200

    Add loader image to images

--- src/components/topic.card/topic.card.container.tsx
+++ src/components/topic.card/topic.card.container.tsx
@@ -4,12 +4,16 @@ import TopicCardView from "./topic.card.view";
 import * as myWikiService from "../../services/my.wiki";
 import { EXAMPLE_WIKI_ID } from "../../global.consts";
 import noImage from "../../assets/images/no_image.jpg";
+import loadingImage from "../../assets/images/loader.gif";
 
 const TopicCardContainer: React.FC<{ topicData: Topic }> = ({ topicData }) => {
-	const [topicImage, setTopicImage] = useState(noImage);
+	const [topicImage, setTopicImage] = useState(loadingImage);
 
 	useEffect(() => {
-		myWikiService.getImage(EXAMPLE_WIKI_ID, topicData.name).then(setTopicImage);
+		myWikiService
+			.getImage(EXAMPLE_WIKI_ID, topicData.name)
+			.then(setTopicImage)
+			.catch(() => setTopicImage(noImage));
 	}, [topicData]);
 
 	return <TopicCardView topicData={topicData} topicImage={topicImage} />;
--- src/components/topic.card/topic.card.view.tsx
+++ src/components/topic.card/topic.card.view.tsx
@@ -1,10 +1,9 @@
 import "./topic.card.scss";
-import noImage from "../../assets/images/no_image.jpg";
 import { Topic } from "../../schemas/topics";
 
 const TopicCardView: React.FC<{ topicData: Topic; topicImage: string }> = ({ topicData, topicImage }) => (
 	<div className="card">
-		<img src={topicImage ?? noImage} alt="empty"></img>
+		<img src={topicImage} alt="topic"></img>
 		<div>
 			<h3>{topicData.name}</h3>
 			<p>{topicData.birth}</p>
